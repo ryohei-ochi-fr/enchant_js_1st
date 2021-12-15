6b239304debcfe09d0b8e83bf01fef05e7c986e9
ryohei ochi <ryohei.ochi@futurerays.biz>
2021-12-15 22:19:16 +0900

    not working

--- js/a011.js
+++ js/a011.js
@@ -4,17 +4,41 @@ enchant();
 //変数宣言
 var game;
 
+async function fetchData() {
+	console.log("loading midi");
+	var smf = "midi/001_Hatsune_Miku_Tell_Your_World_short.mid";
+	const midi = await Midi.fromUrl(smf);
+
+	// const midiData = fs.readFileSync("test.mid")
+	// const midi = new Midi(midiData)
+
+	console.log("loaded midi");
+};
+
+console.log("load midi");
+fetchData();
+
+
 //Webページが読み込まれたら
 addEventListener('load', function () {
+
+	// console.log("load midi");
+	// fetchData();
+
 	//ゲームオブジェクトの作成
 	var game = new Game(640, 360);
 	game.fps = 30;
 
 	game.preload(
 		// 'midi/001_Hatsune_Miku_Tell_Your_World_short.mid',
+		// 'midi/bach_846.mid',
 		'mp3/taiko04.mp3',
 		'img/s100_sample.png'
 	);
+
+	// console.log("load assets midi");
+	// const midiData = game.assets['midi/001_Hatsune_Miku_Tell_Your_World_short.mid']
+	// const midi = new Midi(midiData)
 
 	game.onload = function () {
 		// 背景を生成
@@ -69,117 +93,97 @@ addEventListener('load', function () {
 		});
 
 		game.rootScene.addChild(status);
-	}
+
+		// ここからMIDIファイルパースの検証 Tone.js
+		var label = new Label('MidiPaeser');
+		label.x = 300;
+		game.rootScene.addChild(label);
+
+		// var synths = new Tone.Synth().toMaster();
+
+		// タッチイベントを設定
+		// document.querySelector('#btnPlay').addEventListener('click', (e) => {
+		// label.addEventListener(Event.TOUCH_START, function (e) {
+		// label.addEventListener(Event.TOUCH_START, (e) => {
+		// 	console.log("midi load start");
+
+		// 	enchant.Deferred.next(function () {
+		// 		console.log("called function");
+		// 		var smf = "midi/001_Hatsune_Miku_Tell_Your_World_short.mid";
+		// 		const midi = Midi.fromUrl(smf)
+		// 	})
+
+		// 	// var synth = new Tone.PolySynth(16).toMaster();
+
+		// 	// MidiConvert.load(smf, function(midi) {
+		// 	// 	console.log("called midiconvert");
+
+		// 	// });
+		// });
+		// const synths = []
+
+		// 	Midi.fromUrl(smf).then(midi => {
+		// 		//synth playback
+		// 		// const playing = e.detail
+		// 		const playing = true;
+
+		// 		if (playing) {
+
+		// 			// パースしたmidiデータを表示する
+		// 			console.log(midi);
+
+		// 			// 時間単位(分解能) ppq = 480
+		// 			console.log(midi.header.ppq);
+		// 			console.log(midi.header.tempos[0].bpm);
+
+		// 			midi.tracks.forEach(track => {
+		// 				// トラックの情報を書き出す
+		// 				// console.log(track.channel, track.instrument.name, track.instrument.number);
+		// 				console.log(track);
+
+		// 				track.notes.forEach(note => {
+		// 					// midiの1音のデータ(ノーツ)を書き出してみる
+		// 					// console.log(note.midi, note.time, note.duration, note.name);
+
+		// 				})
+		// 			});
+
+
+		// 			// // Tone.js で音を鳴らす
+		// 			// const now = Tone.now() + 0.5
+		// 			// midi.tracks.forEach(track => {
+		// 			// 	//create a synth for each track
+		// 			// 	const synth = new Tone.PolySynth(10, Tone.Synth, {
+		// 			// 		envelope: {
+		// 			// 			attack: 0.02,
+		// 			// 			decay: 0.1,
+		// 			// 			sustain: 0.3,
+		// 			// 			release: 1
+		// 			// 		}
+		// 			// 	}).toMaster()
+		// 			// 	synths.push(synth)
+		// 			// 	//schedule all of the events
+		// 			// 	track.notes.forEach(note => {
+		// 			// 		synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity)
+		// 			// 	})
+		// 			// })
+		// 		} else {
+		// 			//dispose the synth and make a new one
+		// 			while (synths.length) {
+		// 				const synth = synths.shift()
+		// 				synth.dispose()
+		// 			}
+		// 		}
+		// 	})
+		// })
+
+	};
+	// ここまでMIDIファイルパース
+
+
 
 	// ゲームスタート
 	game.start();
 
 
-
-
-
-	/*	
-	// midiファイルを読み込んでみる
-	var fileReader = new FileReader();
-	fileReader.onload = function () {
-		console.log(fileReader.result);	// ArrayBuffer
-	}
-	// fileReader.readAsArrayBuffer('midi/001_Hatsune_Miku_Tell_Your_World_short.mid') ;
-	// var arrayBuffer = ...;
-	// var data = new StandardMidiFile(new KaitaiStream(arrayBuffer));
-
-	var xhr = new XMLHttpRequest();
-	// xhr.open('GET', 'img/s100_sample.png', true);
-	xhr.open('GET', 'midi/001_Hatsune_Miku_Tell_Your_World_short.mid', true);
-	
-	xhr.responseType = 'arraybuffer';
-
-	xhr.onload = function (e) {
-		if (this.status == 200) {
-			// Note: .response instead of .responseText
-			var blob = new Blob([this.response], { type: 'image/png' });
-			fileReader.readAsArrayBuffer(blob);
-			var data = new StandardMidiFile(new KaitaiStream(fileReader));
-			
-		}
-	};
-
-	xhr.send();
-	*/
-
-	sample().then(result => {
-		console.log(result); // => 15
-	});
-	
-	// const midi = await Midi.fromUrl("path/to/midi.mid")
-
-	// Midi.fromUrl("midi/001_Hatsune_Miku_Tell_Your_World_short.mid").then(midi => {
-	// 	console.log(midi);
-
-	// 	//the file name decoded from the first track
-	// 	const name = midi.name
-	// 	console.log(name);
-	
-	// 	//get the tracks
-	// 	midi.tracks.forEach(track => {
-	// 		//tracks have notes and controlChanges
-	
-	// 		//notes are an array
-	// 		const notes = track.notes
-	// 		notes.forEach(note => {
-	// 			//note.midi, note.time, note.duration, note.name
-	// 			console.log(note.time);
-	// 		})
-	
-	// 		//the control changes are an object
-	// 		//the keys are the CC number
-	// 		track.controlChanges[64]
-	// 		//they are also aliased to the CC number's common name (if it has one)
-	// 		track.controlChanges.sustain.forEach(cc => {
-	// 			// cc.ticks, cc.value, cc.time
-	// 		})
-	
-	// 		//the track also has a channel and instrument
-	// 		//track.instrument.name
-	// 	})
-	// });
-	
 });
-
-/*
-async function sample() {
-	// const midi = new Midi();
-
-	// load a midi file in the browser
-	const midi = await Midi.fromUrl("midi/001_Hatsune_Miku_Tell_Your_World_short.mid")
-	console.log(midi);
-
-	//the file name decoded from the first track
-	const name = midi.name
-	console.log(name);
-
-	//get the tracks
-	midi.tracks.forEach(track => {
-		//tracks have notes and controlChanges
-
-		//notes are an array
-		const notes = track.notes
-		notes.forEach(note => {
-			//note.midi, note.time, note.duration, note.name
-			console.log(note.time);
-		})
-
-		//the control changes are an object
-		//the keys are the CC number
-		track.controlChanges[64]
-		//they are also aliased to the CC number's common name (if it has one)
-		track.controlChanges.sustain.forEach(cc => {
-			// cc.ticks, cc.value, cc.time
-		})
-
-		//the track also has a channel and instrument
-		//track.instrument.name
-	})
-
-}
-*/
